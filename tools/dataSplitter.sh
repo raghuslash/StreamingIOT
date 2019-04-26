370d7a3925163c44fb1e858ec12200801966bab3
Raghunath <raghu@caramail.com>
2019-04-26 16:27:57 +0530

    autocommit by raghu

--- tools/dataSplitter.sh
+++ tools/dataSplitter.sh
@@ -1,12 +1,15 @@
 #for device in "loader" "screenprinter_vaf" "pickandplace1" "pickandplace2" "reflowoven_vaf" "bakingoven1" "bakingoven2"
 
-
-fline=$(head -1 out.csv)
+fname=$1
+if [ -z $1 ] ; then 
+	fname="out.csv"
+fi
+fline=$(head -1 $fname)
 for device in "loader" "screenprinter_plus_vaf" "reflowoven_vaf" "pickandplace1" "pickandplace2"
 
 do	
         echo "Grepping.... $device"
-        grep $device out.csv > temp
+        grep $device $fname > temp
 	echo "Sorting... $device"
         sort -t, -nk2 temp -o "$device.csv"
         rm temp
